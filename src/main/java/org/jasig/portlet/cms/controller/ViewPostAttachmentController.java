0484b51f2daca4ac7fa180a6a9fe45aed51cb1d0
Misagh Moayyed <misagh_moayyed@hotmail.com>
2011-08-03 09:42:09 +0000

    INC-15 Update POM with the maven license plugin, added missing license header to a number of Java classes
    git-svn-id: https://source.jasig.org/portlets/ContentManagementPortlet/trunk@24517 f5dbab47-78f9-eb45-b975-e544023573eb

--- src/main/java/org/jasig/portlet/cms/controller/ViewPostAttachmentController.java
+++ src/main/java/org/jasig/portlet/cms/controller/ViewPostAttachmentController.java
@@ -1,3 +1,15 @@
+/*
+ * Licensed to Jasig under one or more contributor license agreements. See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. Jasig licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at:
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.jasig.portlet.cms.controller;
 
 import java.util.Iterator;
@@ -16,61 +28,61 @@ import org.springframework.web.portlet.mvc.AbstractController;
 
 public class ViewPostAttachmentController extends AbstractController {
 	private final Log logger = LogFactory.getLog(getClass());
-
+	
 	private RepositoryDao repositoryDao = null;
-
-
-	public void setRepositoryDao(final RepositoryDao repositoryDao) {
-		this.repositoryDao = repositoryDao;
-	}
-
+	
+	
 	private RepositoryDao getRepositoryDao() {
 		return repositoryDao;
 	}
-
+	
 	@Override
 	protected void handleActionRequestInternal(final ActionRequest request, final ActionResponse response) throws Exception {
-
+		
 		final String attachmentPath = PortletRequestUtils.getRequiredStringParameter(request, "attachmentPath");
 		final String postPath = PortletRequestUtils.getRequiredStringParameter(request, "postPath");
-
+		
 		if (logger.isDebugEnabled())
 			logger.debug("Attachment path to remove: " + attachmentPath);
-
+		
 		final Post post = getRepositoryDao().getPost(postPath);
 		if (post != null) {
-
+			
 			if (logger.isDebugEnabled())
 				logger.debug("Retrieved repository post " + post);
-
+			
 			final Iterator<Attachment> it = post.getAttachments().iterator();
-
+			
 			Attachment attachment = null;
 			while (it.hasNext()) {
 				attachment = it.next();
 				if (attachment.getPath().equals(attachmentPath)) {
 					if (logger.isDebugEnabled())
 						logger.debug("Found post attachment: " + attachment);
-
+					
 					request.getPortletSession()
 					.setAttribute("attachment", attachment, PortletSession.APPLICATION_SCOPE);
-
+					
 					final String contextPath = request.getContextPath();
-
+					
 					final String url = new StringBuilder().append(contextPath).append("/")
-					.append("downloadPostAttachment").toString();
-
+							.append("downloadPostAttachment").toString();
+					
 					if (logger.isDebugEnabled())
 						logger.debug("Url is: " + url);
-
+					
 					response.sendRedirect(url);
 				}
 			}
-
+			
 		}
-
-
+		
+		
 	}
-
-
+	
+	public void setRepositoryDao(final RepositoryDao repositoryDao) {
+		this.repositoryDao = repositoryDao;
+	}
+	
+	
 }
